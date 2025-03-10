5e0e5f441b2fa48198b7c71af009a0c0dd316555
infinitefan22 <infinitefan22@gmail.com>
2025-03-10 17:09:51 -0500

    Add files via upload

--- EP_KeltyPlots.m
+++ EP_KeltyPlots.m
@@ -1,7 +1,12 @@
-% clear ; clc ; close all
+ clear ; clc ; close all
+ %% Design elements and ease of use
+plottingfig1 = 1 ;
+plottingfig2 = 0 ; 
+% set(0, 'DefaultAxesFontName', 'Nimbus Roman', 'DefaultTextFontName', 'Nimbus Roman')
+% set(0,'defaultTextInterpreter','latex')
 %% Setting Constants and Loading Data 
 CNT = 1 ; 
-% load('aalldata_Mar062025DELETE.mat') ;
+load('aalldata_Mar062025DELETE.mat') ;
 
 offset = [3.0 3.9 2.8 3.5 3.8 3.8 ]; %this line is why j can only go to 6
 
@@ -39,9 +44,14 @@ numofTrials = length(xi) ;
 % eta = ...
 % etc/. 
 %% for loops 
+
 for num = 1:length(xi)
- [j1, j2] = min(abs(xi{num}-43));
+    %%
+    trialnum = num ; titlename = join([strrep(categoryname, '_', '-'), ' Trial ', string(trialnum), ' H_r_m_s=', sprintf('%2.2f',Hrmsi{num}(1))], "") ;
+    [j1, j2] = min(abs(xi{num}-43));
     j = num ; 
+
+    if plottingfig1 == 1
     figure(CNT);clf;clear hh hl;
          CNT=CNT+1 ; % CLARA
          subplot(2,1,1)
@@ -51,7 +61,8 @@ for num = 1:length(xi)
          hh1(2) = plot(xwg{num},sqrt(8)*std(eta{num}),'rs-','markerfacecolor','r');hold on
          hh1(3) = plot(xi{num},modelHrms{num},'k-','linewidth',2);hold on ; 
          hh1(3) = plot(xi{num},datHrms{num},'k--','linewidth',2);hold on      %dname2=dname;dname2(dname=='_')='-';
-         title([strrep(categoryname,'_','-','Trial ', num),' ',sprintf('%2.2f',Hrmsi{num}(1))],'interpreter','latex','fontsize',12) ; %,' ',sprintf('%2.2f',stats.Tp)]
+         title(titlename);
+%          title(categoryname+'_Trial '+string(trialnum),' ',sprintf('%2.2f',Hrmsi{num}(1)),'interpreter','latex','fontsize',12) ; %,' ',sprintf('%2.2f',stats.Tp)]
         %xlabel('$x[m]$','interpreter','latex','fontsize',16)
          ylabel('$H_{rms}[m]$','interpreter','latex','fontsize',16)
          a = axis;
@@ -72,9 +83,10 @@ for num = 1:length(xi)
           %plot(xi,1000*etaiwavesbotshearwrs,'m--','linewidth',2);
           %plot(xi,1000*etaiwavesbotshearveg2p,'m-','linewidth',2);
           %hh(2) = plot(xi,1000*etaiwavesbotshearveg2,'k-','linewidth',3);
-          hl = legend(hh,'Modeled $ \eta, u $','Measued $\eta, u $','${\bf F}= 0$','AutoUpdate','off');
+          legend(hh,'Modeled  \eta, u ','Measured \eta, u','{\bf F}= 0','AutoUpdate','off');
+%           hl = legend(hh,'Modeled $ \eta, u $','Measued $\eta, u $','${\bf F}= 0$','AutoUpdate','off');
           %hl = legend(hh,'$S_{xx}, \tau_b$','$S_{xx}, \tau_b, {\bf F}$','AutoUpdate','off');
-          set(hl,'interpreter','latex','fontsize',14,'location','northwest')
+%           set(hl,'interpreter','latex','fontsize',14,'location','northwest')
           dumyl = ylim;
           if dumyl(2)-dumyl(1)<100
             ylim([mean(ylim)-40 mean(ylim)+40])
@@ -82,51 +94,53 @@ for num = 1:length(xi)
           xlabel('$x[m]$','interpreter','latex','fontsize',16)
           ylabel('$\overline{\eta}[mm]$','interpreter','latex','fontsize',14)
           set(gca,'TickLabelInterpreter','latex') ;
-    
-%       dname2=dname;dname2(dname=='_'|dname=='/')='-';
+
+%       dname2=categoryname;dname2(categoryname=='_'|categoryname=='/')='-';
 %     %   print('-dpng',[dname2(21:end-1),'.png']) 
-%       figname = [basename, '_' ,  dnames(jj).name] ;   % should give you  Baseline_h158_hv073_NoWall_Trial01'
+%       figname = [categoryname, '_Trial ' , trialnum] ;   % should give you  Baseline_h158_hv073_NoWall_Trial01'
 %       print('-dpng',[figname,'.png']);
 %     %   a = axis;
 %     %   hf=fill([36 54 54 36],[a(3) a(3) a(4) a(4)],[.2 .5 .2]);set(hf,'facealpha',.2)
-%     %   axis(a)
-  
+    %   axis(a)
+    end
 
-%   figure(CNT);clf
-%       CNT=CNT+1 ; % CLARA
-%       subplot(3,1,1)
-%       plot(t,udum,'r','linewidth',2);hold all
-%         plot(t,0*udum,'k','linewidth',1);hold all
-%       plot(sav(j2).t+offset(j),mean(sav(j2).u),'b','linewidth',2)
-%       ylabel('$u$','interpreter','latex','fontsize',16)
-%       %xlabel('$t[s]$','interpreter','latex','fontsize',16)
-%       xlim([0 20]);
-%       title([strrep(dname(21:end),'_','-'),' ',sprintf('%2.2f',Hrmsi(1)),' ',sprintf('%2.2f',stats.Tp)],'interpreter','latex','fontsize',12)
-%       %print('-dpng',[dname2(21:end-1),'u.png'])
-%     
-%       subplot(3,1,2)
-%       plot(t,-d,'r','linewidth',2);hold all
-%       plot(t,0*t+mean(-d),'r--','linewidth',1);hold all
-%       plot(sav(j2).t+offset(j),-sav(j2).Dx,'b','linewidth',2)
-%       plot(sav(j2).t+offset(j),ones(size(sav(j2).Dx))*mean(-sav(j2).Dx),'b--','linewidth',1)
-%       ylabel('$-D$','interpreter','latex','fontsize',16)
-%       %xlabel('$t[s]$','interpreter','latex','fontsize',16)
-%       xlim([0 20]);
-%       %title([strrep(dname(21:end),'_','-'),' ',sprintf('%2.2f',Hrmsi(1)),' ',sprintf('%2.2f',stats.Tp)],'interpreter','latex','fontsize',12)
-%       %print('-dpng',[dname2(21:end-1),'u.png'])
-%       
-%       subplot(3,1,3)
-%       plot(t,0*F2,'k');hold all
-%       hf(1) = plot(t,F2,'r','linewidth',2);hold all
-%       plot(t,mean(F2)+0*t,'r--');hold all
-%       hf(2) = plot(sav(j2).t+offset(j),sav(j2).Fx,'b','linewidth',2) ; 
-%       plot(sav(j2).t+offset(j),sav(j2).meanFx+0*sav(j2).t,'b--','linewidth',1)
-%       ylabel('$F_x $','interpreter','latex','fontsize',16)
-%       xlabel('$t[s]$','interpreter','latex','fontsize',16)
-%       xlim([0 20]);
-%       %title([strrep(dname(21:end),'_','-'),' ',sprintf('%2.2f',Hrmsi(1)),' ',sprintf('%2.2f',stats.Tp)],'interpreter','latex','fontsize',12)
-%       hl = legend(hf,'Using Measured Data','Using Modeled Data','AutoUpdate','off');
-%       set(hl,'interpreter','latex','fontsize',14,'location','northeast') ;
-%     %  print('-dpng',[dname2(21:end-1),'_u_d_F.png']) EDITED CLARA
+if plottingfig2 ==1
+  figure(CNT);clf
+      CNT=CNT+1 ; % CLARA
+      subplot(3,1,1)
+      plot(t,udum,'r','linewidth',2);hold all
+        plot(t,0*udum,'k','linewidth',1);hold all
+      plot(sav(j2).t+offset(j),mean(sav(j2).u),'b','linewidth',2)
+      ylabel('$u$','interpreter','latex','fontsize',16)
+      %xlabel('$t[s]$','interpreter','latex','fontsize',16)
+      xlim([0 20]);
+      title([strrep(dname(21:end),'_','-'),' ',sprintf('%2.2f',Hrmsi(1)),' ',sprintf('%2.2f',stats.Tp)],'interpreter','latex','fontsize',12)
+      %print('-dpng',[dname2(21:end-1),'u.png'])
+    
+      subplot(3,1,2)
+      plot(t,-d,'r','linewidth',2);hold all
+      plot(t,0*t+mean(-d),'r--','linewidth',1);hold all
+      plot(sav(j2).t+offset(j),-sav(j2).Dx,'b','linewidth',2)
+      plot(sav(j2).t+offset(j),ones(size(sav(j2).Dx))*mean(-sav(j2).Dx),'b--','linewidth',1)
+      ylabel('$-D$','interpreter','latex','fontsize',16)
+      %xlabel('$t[s]$','interpreter','latex','fontsize',16)
+      xlim([0 20]);
+      %title([strrep(dname(21:end),'_','-'),' ',sprintf('%2.2f',Hrmsi(1)),' ',sprintf('%2.2f',stats.Tp)],'interpreter','latex','fontsize',12)
+      %print('-dpng',[dname2(21:end-1),'u.png'])
+      
+      subplot(3,1,3)
+      plot(t,0*F2,'k');hold all
+      hf(1) = plot(t,F2,'r','linewidth',2);hold all
+      plot(t,mean(F2)+0*t,'r--');hold all
+      hf(2) = plot(sav(j2).t+offset(j),sav(j2).Fx,'b','linewidth',2) ; 
+      plot(sav(j2).t+offset(j),sav(j2).meanFx+0*sav(j2).t,'b--','linewidth',1)
+      ylabel('$F_x $','interpreter','latex','fontsize',16)
+      xlabel('$t[s]$','interpreter','latex','fontsize',16)
+      xlim([0 20]);
+      %title([strrep(dname(21:end),'_','-'),' ',sprintf('%2.2f',Hrmsi(1)),' ',sprintf('%2.2f',stats.Tp)],'interpreter','latex','fontsize',12)
+      hl = legend(hf,'Using Measured Data','Using Modeled Data','AutoUpdate','off');
+      set(hl,'interpreter','latex','fontsize',14,'location','northeast') ;
+    %  print('-dpng',[dname2(21:end-1),'_u_d_F.png']) EDITED CLARA
+end
 
 end
