cb53cee9d6f15ae483b5f4dac216cd3e70f25ef9
infinitefan22 <infinitefan22@gmail.com>
2025-04-09 16:00:33 -0500

    Add files via upload

---
+++ ClaraFunctions/set_category_variables.m
@@ -0,0 +1,36 @@
+clear alpha Cdexct2 CdKelty d datEf datHrms eta eta_init eta_p eta0a eta0b F2 F2overCd Hrmsi hv 
+clear KC modeleta modelHrmsp p_init sav stats t u udum waveperiod xi xp xwg
+ KC = aalldata.(categoryname).KC ; 
+   modeleta = aalldata.(categoryname).modeleta ;
+   modelHrms = aalldata.(categoryname).modelHrms ;
+   p = aalldata.(categoryname).p ; 
+   p_init = aalldata.(categoryname).p_init ; 
+%    Re = aalldata.(categoryname).Re ; 
+   sav = aalldata.(categoryname).sav ; 
+   stats = aalldata.(categoryname).stats ; 
+   t = aalldata.(categoryname).t ; 
+%    w = aalldata.(categoryname).w ; 
+   u = aalldata.(categoryname).u ; 
+   udum = aalldata.(categoryname).udum ; 
+   waveperiod = aalldata.(categoryname).waveperiod ; 
+   xi = aalldata.(categoryname).xi ; 
+   xp = aalldata.(categoryname).xp ; 
+   xwg = aalldata.(categoryname).xwg ; 
+%    zw = aalldata.(categoryname).zw ;    
+alpha = aalldata.(categoryname).alpha ; 
+   Cdexact2 = aalldata.(categoryname).Cdexact2 ; 
+   CdKelty = aalldata.(categoryname).CdKelty ; 
+   d = aalldata.(categoryname).d ; 
+   datEf = aalldata.(categoryname).datEf ; 
+   dateta = aalldata.(categoryname).dateta ;
+   datHrms = aalldata.(categoryname).datHrms ; 
+   eta = aalldata.(categoryname).eta ; 
+   eta_init = aalldata.(categoryname).eta_init ; 
+   eta_p = aalldata.(categoryname).eta_p ; 
+   eta0a = aalldata.(categoryname).eta0a ; 
+   eta0b = aalldata.(categoryname).eta0b ; 
+   F2 = aalldata.(categoryname).F2 ; 
+   F2overCd = aalldata.(categoryname).F2overCd ; 
+   Hrmsi = aalldata.(categoryname).Hrmsi ; 
+   hv = aalldata.(categoryname).hv ; 
+  
