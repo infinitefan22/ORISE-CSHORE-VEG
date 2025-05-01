ea15dc0c28a77d53bc09f733e65ff0cd82ba31b7
infinitefan22 <infinitefan22@gmail.com>
2025-05-01 14:39:25 -0500

    Add files via upload

--- ClaraFunctions/matchB_HDLDTrials.m
+++ ClaraFunctions/matchB_HDLDTrials.m
@@ -1,5 +1,7 @@
-function matchB_HDLDTrials(aalldata, categoryname, num)
-
+function [fbaseline, tnumb] = matchB_HDLDTrials(aalldata, categoryname, num, Tprange, Hrmsirange) 
+clear tnumb fbaseline ogwavetype ogTp ogHrmsi fbwavetype fbTp fbHrmsi 
+% Tprange = .5
+% Hrmsirange = .05
 if contains(categoryname, 'h158')
     fbaseline = 'Baseline_h158_hv073_NoWall' ; 
 elseif contains(categoryname, 'h188')
@@ -12,22 +14,58 @@ else
     disp("invalid categoryname")
 end
 
-ogTp = aalldata.(categoryname).Tp{num} ; 
-ogHrmsi = aalldata.(categoryname).Hrmsi{num} ; 
-ogwavetype = aalldata.(categoryname).wavetype{num} ; 
+tnumb = [] ; 
 
-fbTp = aalldata.(fbaseline).Tp ; 
-fbHrmsi = aalldata.(fbaseline).Hrmsi ; 
+ogwavetype = aalldata.(categoryname).wavetype{num} ; 
+ogTp = aalldata.(categoryname).Tp{num} ; 
+ogHrmsi = rms(aalldata.(categoryname).Hrmsi{num}) ; 
+
 fbwavetype = aalldata.(fbaseline).wavetype ; 
+fbTp = aalldata.(fbaseline).Tp ; 
+        for ii = 1:length(aalldata.(fbaseline).Hrmsi)
+fbHrmsi{ii} = rms(aalldata.(fbaseline).Hrmsi{ii}) ; %this is inside for loop
+        end
+
 
 for jj = 1:length(aalldata.(fbaseline).Tp)
-if aalldata.(fbaseline).wavetype{jj} == aalldata.(categoryname).wavetype{num} ; 
-
-if idxwavetype == 0
-    disp("wavetype match not found")
+if contains(fbwavetype{jj},ogwavetype)
+    if ogTp < fbTp{jj} + Tprange && ogTp > fbTp{jj} - Tprange
+        if ogHrmsi < fbHrmsi{jj} + Hrmsirange && ogHrmsi > fbHrmsi{jj} - Hrmsirange
+            if isempty(tnumb)
+                tnumb(1) = jj ; 
+            else 
+                tnumb(end+1) = jj ; 
+            end
+        end
+    end
 end
+end 
 
-idxTp =  == ogTp ; 
+% if isempty(tnumb)
+%     disp("no Baseline trial matches could be found for " + categoryname + " Trial " + num + ". Factors considered: wavetype, Tp, Hrmsi")
+% end
+ clear weighteddif ; weighteddif = [] ; 
+
+if length(tnumb) > 1
+    for kk = 1:length(tnumb)
+        difTp = abs(ogTp - fbTp{tnumb(kk)}) ; 
+        difHrmsi = abs(ogHrmsi - fbHrmsi{tnumb(kk)}) ; 
+        if isempty(weighteddif)
+            weighteddif(1) = difTp/Tprange + difHrmsi/Hrmsirange ; 
+        else 
+            weighteddif(end+1) = difTp/Tprange + difHrmsi/Hrmsirange ;  
+        end
+    end
+
+    [~, idxn] = min(weighteddif) ;
+    tnumb = tnumb(idxn) ; 
+elseif isscalar(tnumb)
+    idxn = 1 ; 
+    tnumb = tnumb(idxn) ; 
+else 
+     disp("no Baseline trial matches could be found for " + categoryname + " Trial " + num + ". Factors considered: wavetype, Tp, Hrmsi")
+     tnumb = 0 ; 
+end
 
 
 end %idk if this will end up being a function, but we'll see
