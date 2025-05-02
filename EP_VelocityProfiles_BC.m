de14308c4a18036fe9916e36beec6c093ae1afcb
infinitefan22 <infinitefan22@gmail.com>
2025-05-02 15:11:40 -0500

    Add files via upload

--- EP_VelocityProfiles_BC.m
+++ EP_VelocityProfiles_BC.m
@@ -16,7 +16,7 @@ savfolderpath = join([currentfolder, '/ClaraFigures/EP_VelocityProfiles/20250501
 cnt =1;
 clear fieldnames ; fieldnames = fieldnames(aalldata) ;
 zu = [1.4040, 1.5500, 1.7200, 1.8580] ; % ADV 2,3,4,5 (order is correct and checked)
-% bmatch = zeros(1, length(fieldnames)) ; 
+catcolors = hsv(length(fieldnames)-4) ; 
 for totalnum = 1:length(fieldnames)
     categoryname = fieldnames{totalnum} ; %'HighDensity_h270_hv182_NoWall' ; %
     set_category_variables
@@ -60,21 +60,53 @@ trialcolors = hsv(NumofTrials) ;
 clear savfigname ; savfigname = join(['VelocityProfilesMatched_',categoryname, '.png'],'') ; % , 'Trial',string(num)
 end 
 %% Plotting 
-figure(cnt) ; cnt=cnt+1 ;
+% figure(cnt) ; cnt=cnt+1 ;
+%     for num = 1:NumofTrials
+%         if bmatch.(categoryname).match(num) == 0
+%             scatter(difvrmstrial1.(categoryname)(num,:), zu, 55, trialcolors(num,:), 'x', 'filled', 'MarkerEdgeColor', trialcolors(num,:)) ; hold on 
+%         else
+%         scatter(difvrmstrial1.(categoryname)(num,:), zu, 36, trialcolors(num,:), 'o', 'filled', 'MarkerEdgeColor', "black") ; hold on 
+%         end
+%     end
+%     % scatter(vrmscategories.(categoryname), zu, 100, 'black', 'filled') %RMS
+%     scatter(difvmeancategories2.(categoryname), zu, 100, 'black', 'filled', 'MarkerEdgeColor', "black") %Mean not including x's
+%     legend(trialnumbers)
+%     ylim([1, 3])
+%     xlabel('$u_{mean}$ (m/s)')
+%     ylabel('Elevation (m)')
+%     title(join([titlename, ' $v_{mean}$'],''))
+%     set(gcf, 'Position', [10, 10, 600, 400]);
+%     % saveas(gcf, fullfile(savfolderpath, savfigname))
+
+end
+%%
+figure(100)
+for totalnum = 5:length(fieldnames)
+    categoryname = fieldnames{totalnum} ;
+    set_category_variables
+    NumofTrials = length(t) ; 
+    bmatch.(categoryname).match = ones(1,NumofTrials) ; 
+    % figure(100) ; 
     for num = 1:NumofTrials
         if bmatch.(categoryname).match(num) == 0
-            scatter(difvrmstrial1.(categoryname)(num,:), zu, 55, trialcolors(num,:), 'x', 'filled', 'MarkerEdgeColor', trialcolors(num,:)) ; hold on 
+            scatter(difvrmstrial1.(categoryname)(num,:), zu, 55, catcolors(totalnum-4,:), 'x', 'filled', 'MarkerEdgeColor', white) ; hold on 
         else
-        scatter(difvrmstrial1.(categoryname)(num,:), zu, 36, trialcolors(num,:), 'o', 'filled', 'MarkerEdgeColor', "black") ; hold on 
+        scatter(difvrmstrial1.(categoryname)(num,:), zu, 36, 'white', 'square', 'filled', 'MarkerEdgeColor', "white") ; hold on 
         end
     end
-    % scatter(vrmscategories.(categoryname), zu, 100, 'black', 'filled') %RMS
-    scatter(difvmeancategories2.(categoryname), zu, 100, 'black', 'filled', 'MarkerEdgeColor', "black") %Mean not including x's
-    legend(trialnumbers)
+end
+
+for totalnum = 5:length(fieldnames)
+    categoryname = fieldnames{totalnum} ;
+    set_category_variables
+    NumofTrials = length(t) ; 
+    bmatch.(categoryname).match = ones(1,NumofTrials) ; 
+    gg1(totalnum-4) = scatter(difvmeancategories2.(categoryname), zu, 100, catcolors(totalnum-4,:), 'o', 'filled', 'MarkerEdgeColor', "black") ; %Mean not including x's
     ylim([1, 3])
     xlabel('$u_{mean}$ (m/s)')
     ylabel('Elevation (m)')
-    title(join([titlename, ' $v_{mean}$'],''))
+    title('HD/LD $v_{mean}$ of all trials')
     set(gcf, 'Position', [10, 10, 600, 400]);
-    saveas(gcf, fullfile(savfolderpath, savfigname))
+    % saveas(gcf, fullfile(savfolderpath, savfigname))
 end
+legend(gg1, {'High Density h158 hv070', 'High Density h188 hv100', 'High Density h270 hv182', 'Low Density h158 hv073', 'Low Density h188 hv103', 'Low Density h233 hv148', 'Low Density h270 hv185'})
